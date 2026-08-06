9e052637bdfbbe8ad19b731ee55018c5300053a3
lit leprichorn <tylerhooper380@gmail.com>
2026-08-06 21:07:16 +0100

    Version 1.1
    Added value list thing.
    
    
    
    fuck

--- Custom_Language.py
+++ Custom_Language.py
@@ -1,4 +1,10 @@
+valist = []
+import random
 i=0
+total=0
+
+from lang_xtras import allowed, resp
+
 while True:
     file = input("File: ")
     file = file + ".pyrantic"
@@ -11,13 +17,34 @@ while True:
                 if len(parts)>1:
                     function = parts[0].strip()
                     item = parts[1].strip()
-                   
+                    #print(function)
+                    #print({repr(function)})
                     if function == "p":
                         print(item)
-                    if function == "math":
+                    elif function == "math":
+                        if not all(chars in allowed for chars in item):# checks if every character is isnide of allowed list
+                            print("fuck. not again. ")
+                            print(item, " ", random.choice(resp))
+                            continue
                         try:
                             math_result = eval(item)
                             print(math_result)
                         except:
                             print(item)
-            
+                            
+                    elif function == "d":
+                        with open(file,"w") as destroy:
+                            for line in file:
+                                total += 1
+                                for _ in range(total):
+                                    destroy.write(f"{total}\n Where did it go?")
+                    elif function == "val":
+                        print("val is running bro") # WHY ARE YOU NOT RUNNING?????,nvm fixed
+                        valist.extend(item.split(' '))
+                        #print(valist)
+                    
+                    
+                    
+                    
+                    
+                    
